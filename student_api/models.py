fe8fb4162c58c4eafd104b9556e69d5ebbeb97e5
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-22 22:53:34 +0300

    permissions.py was created

--- student_api/models.py
+++ student_api/models.py
@@ -4,7 +4,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField(blank=True, null=True)
-    user=
+    user=models.ForeignKey(User, on_delete=CASCADE)
 
 
     def __str__(self):
---
+++ student_api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+class IsAdminorReadOnly(permissions.IsAdminUser):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return bool(request.user.is_staff)
